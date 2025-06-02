7877a5788ac67451c69ec3c6b54e5b0db36b978e
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2025-06-02 21:38:52 +0900

    chore: set up Jest testing framework for TDD approach
    - Added Jest and testing libraries (@testing-library/react, @testing-library/jest-dom)
    - Configured Jest for Next.js with TypeScript support
    - Added test scripts to package.json (test, test:watch, test:coverage)
    - Created jest.config.js with module path mappings
    - Set up jest.setup.js with testing-library/jest-dom imports

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.(test|spec).(ts|tsx|js|jsx)',
+    '**/src/**/*.(test|spec).(ts|tsx|js|jsx)',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
